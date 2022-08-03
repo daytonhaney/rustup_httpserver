9cab4ca9ecad31091d0540a1e016df45be203d65
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-08-03 02:29:45 -0400

    Update server.rs

--- rust-scratch-http-server/server.rs
+++ rust-scratch-http-server/server.rs
@@ -1,1 +1,35 @@
-r
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,          // stopped at Match expression 
+}
+impl Server {
+    pub fn new(addr: String) ->Self{
+        Self {addr}
+    }
+    pub fn run(self){
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((stream,addr))=>{
+                    let a = 5;
+                    println!("OK")
+              
+                }
+                Err(e) => println!("Err",)
+            }
+            let res = listener.accept();
+            if res.is_err() {
+                continue; 
+                
+            }
+            
+            let (stream,addr) = res.unwrap();
+
+        }
+    }
+
+}
+Footer
